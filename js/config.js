bb08fbc693cc85a805505044aec44b8c12311a04
RemorqOperations <operations.remorq@gmail.com>
2026-03-12 11:42:55 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyI0-58F_17e3rD5-CQAyW6sDMyWbqGaIGL-tT43QAkqSJDXGg2WYZUAr2aq7L-27QdQA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxv0Szvgdpohs5sPUElFLamvjQXYRHwXK-MTSzutMSChzidF1bn1auJuoicOZhKv0GP/exec";
